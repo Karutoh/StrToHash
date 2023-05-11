600e9e5947586dfed0d79cce3d76cddc6988e5c7
Karutoh <NelsonArron@outlook.com>
2023-05-10 20:32:05 -0700

    Backup.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,27 @@
 #include <EHS/EHS.h>
 #include <EHS/Str.h>
-#include <EHS/Log.h>
-#include <EHS/Array.h>
-#include <EHS/Vector.h>
-#include <EHS/IO/MsgBox.h>
 #include <EHS/IO/Console.h>
 
 int main()
 {
-    if (!ehs::Console::Attach())
-        ehs::Console::Create();
+    ehs::Console::Attach();
 
     ehs::Str_8 cmd = ehs::Console::GetCommandLine_8();
 
-    ehs::USize s = 0;
+    ehs::UInt_64 s = 0;
 
     if (cmd.Find("  ", &s, ehs::SearchPattern::LEFT_RIGHT, ehs::IndexResult::ENDING))
     {
         ehs::Str_8 fullArgs = cmd.Sub(s);
         ehs::Vector<ehs::Str_8> args = fullArgs.Split(" ");
-        for (ehs::USize i = 1; i < args.Size(); ++i)
-            ehs::Console::Write_8("Result " + ehs::Str_8::FromNum(i) + ": " + ehs::Str_8::FromNum(args[i].Hash()));
+        for (ehs::UInt_64 i = 1; i < args.Size(); ++i)
+            ehs::Console::Write_8("Result " + ehs::Str_8::FromNum(i) + ": " + ehs::Str_8::FromNum(args[i].Hash_64()));
     }
     else
     {
         ehs::Console::Write_8("String: ", false);
         ehs::Str_8 in = ehs::Console::Read_8();
-        ehs::Console::Write_8("Result: " + ehs::Str_8::FromNum(in.Hash()));
+        ehs::Console::Write_8("Result: " + ehs::Str_8::FromNum(in.Hash_64()));
 
         ehs::Console::Read_8();
     }
